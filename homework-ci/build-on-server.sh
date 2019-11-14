9c94e46c28d8df29aa047c4e4507f3b4153814ee
alsam1992 <alsam1992@yandex.ru>
2019-11-14 20:14:05 +0700

    test travis13

--- homework-ci/build-on-server.sh
+++ homework-ci/build-on-server.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update -y
 sudo apt-get install python3-pip -y
-python3 -mpip install -r requirements.txt
+sudo python3 -mpip install -r requirements.txt
 sudo Xvfb :99 -ac &
 sudo python3 -mpytest ./homework-ci/test_todo_mvc.py
